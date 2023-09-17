0ac155bcf3045478b7cae91051649817c94c1144
ggm77 <shmshm86@naver.com>
2023-09-17 21:27:11 +0900

    homework 1

---
+++ hw1st/src/main/java/com/pard/hw1st/controller/MainController.java
@@ -0,0 +1,39 @@
+package com.pard.hw1st.controller;
+
+
+// TODO: MainController를 만들어주세요.
+
+import com.pard.hw1st.dto.MyInfoDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String startPage() {
+        return "start";
+    }
+
+    @GetMapping("/submitInfo")
+    public String showForm() {
+        return "submitInfo";
+    }
+
+    @PostMapping("/showInfo")
+    public String newInfoForm(MyInfoDto dto, Model model) {
+
+        model.addAttribute("major",dto.getMajor());
+        model.addAttribute("grade",dto.getGrade());
+        model.addAttribute("age",dto.getAge());
+        model.addAttribute("hobby",dto.getHobby());
+        model.addAttribute("name",dto.getName());
+        model.addAttribute("hometown",dto.getHometown());
+        model.addAttribute("introduction",dto.getIntroduction());
+
+        return "showInfo";
+    }
+
+}
